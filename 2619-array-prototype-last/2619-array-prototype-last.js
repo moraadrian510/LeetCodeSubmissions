3663f0c8252399d735e506fb5efdd7d9bf398f88
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-10-29 16:49:17 -0700

    Time: 46 ms (77.56%), Space: 41.9 MB (21.93%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -1,22 +1,23 @@
 /**
  * @return {null|boolean|number|string|Array|Object}
  */
-Array.prototype.last = function() {
-    if (this.length === 0) {
-        return -1;
-    } else {
-        return this[this.length - 1];
-    }
-}
 
-let myArray1 = [1, 2, 3, 4];
-let myArray2 = [];
+    Array.prototype.last = function() {
+        if ( this.length === 0 ) {
+            return -1
+        } else {
+            return this[this.length  -1]
+        }
+    };
 
-let lastElement1 = myArray1.last();
-let lastElement2 = myArray2.last();
+    let myArray = [1, 2, 3, 4];
+    let myArray2 = [];
 
-console.log(lastElement1);
-console.log(lastElement2);
+    let lastObject = myArray.last();
+    let lastObject2 = myArray2.last();
+
+    console.log(lastObject);
+    console.log(lastObject2);
 
 
 /**
